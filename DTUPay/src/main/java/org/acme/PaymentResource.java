300d4ebb51619d1b27381a26704f021e5ac6a2bb
tiagosilveriom <tiago.ars.machado@gmail.com>
2023-01-07 17:51:41 +0100

    dtu pay

--- DTUPay/src/main/java/org/acme/PaymentResource.java
+++ DTUPay/src/main/java/org/acme/PaymentResource.java
@@ -1,11 +1,14 @@
 package org.acme;
 
+import dtu.ws.fastmoney.User;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/payments")
@@ -34,6 +37,13 @@ public class PaymentResource {
     @Consumes(MediaType.APPLICATION_XML)
     public boolean postPaymentXML(Payment p) {
         return pService.addPayment(p);
+    }
+
+    @POST
+    @Path("/registerUser")
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response registerUserXML(CreateUser data) {
+        return pService.registerUser(data);
     }
 
 }
--- DTUPay/src/main/java/org/acme/PaymentService.java
+++ DTUPay/src/main/java/org/acme/PaymentService.java
@@ -2,9 +2,17 @@ package org.acme;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.core.Response;
+
+import dtu.ws.fastmoney.Account;
+import dtu.ws.fastmoney.BankService;
+import dtu.ws.fastmoney.BankServiceException_Exception;
+import dtu.ws.fastmoney.BankServiceService;
+import dtu.ws.fastmoney.User;
 
 public class PaymentService {
 
+    BankService service = (new BankServiceService()).getBankServicePort();
     private List<Payment> paymentList = new ArrayList<>();
 
     private Payment payment= new Payment("cid1","mid1",100);
@@ -19,4 +27,16 @@ public class PaymentService {
     public boolean addPayment(Payment p) {
         return paymentList.add(p);
     }
+
+    public Response registerUser(CreateUser data) {
+        try {
+            var id = service.createAccountWithBalance(data.user, data.balance);
+            return Response.ok(id).build();
+        } catch (BankServiceException_Exception e) {
+            System.err.println("Got error: " + e.getMessage());
+            e.printStackTrace();
+            return Response.status(Response.Status.PRECONDITION_FAILED).entity(e.getCause()).build();
+        }
+    }
+
 }
